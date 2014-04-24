f4d23ed2ecfbfa86b5a31eececed61e47dcc1ccf
Tuan-Tu TRAN <tuantu.t@gmail.com>
2014-04-24 02:57:39 +0200

    added an init.inc.php file

---
+++ src/inc/init.inc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright (c) 2014 Educ-Action
+ * 
+ * This file is part of ADES.
+ * 
+ * ADES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ADES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ADES.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ * Author: Tuan-Tu Tran
+ *
+ * This file contains basic initializations and should
+ * be safely included at the begining of any controller
+ */
+
+define('_DB_CONFIG_FILE_','config/confbd.inc.php');
+
+require("inc/fonctions.inc.php");
+
+?>
