742e8b43f666b9f72dc65721266560f8d96c676d
Tuan-Tu TRAN <tuantu.t@gmail.com>
2014-04-25 19:36:16 +0200

    added exit statement after redirect

--- src/inc/prive.inc.php
+++ src/inc/prive.inc.php
@@ -18,6 +18,8 @@
  * along with ADES.  If not, see <http://www.gnu.org/licenses/>.
 */
 session_start();
-if (!isset($_SESSION["identification"]["user"]))
+if (!isset($_SESSION["identification"]["user"])){
 	header("Location: accueil.php");
+	exit;
+}
 ?>
