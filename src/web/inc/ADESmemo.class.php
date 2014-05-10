837567f514818e444719bd763a265d50da441873
Tuan-Tu TRAN <tuantu.t@gmail.com>
2014-05-10 03:45:45 +0200

    moved all files to web folder
    + removed some unused files
    + ignore some missing copyrights

--- src/web/inc/ADESmemo.class.php
+++ src/web/inc/ADESmemo.class.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright (c) 2010 Adrien Rami
+ * 
+ * This file is part of ADES.
+ * 
+ * ADES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ADES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ADES.  If not, see <http://www.gnu.org/licenses/>.
+*/
 include ("ADESsql.class.php");
 /**
  * ADESmemo:
