a155d7fa221692f75a859fa833b5f2aab7ebbbf6
Tuan-Tu TRAN <tuantu.t@gmail.com>
2014-05-29 11:57:06 +0200

    enabled twig templating

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,7 @@ class AppKernel extends Kernel
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             //new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            //new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
             //new Symfony\Bundle\MonologBundle\MonologBundle(),
             //new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             //new Symfony\Bundle\AsseticBundle\AsseticBundle(),
