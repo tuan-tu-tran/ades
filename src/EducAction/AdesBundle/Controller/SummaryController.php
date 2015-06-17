69c22d745a9b4f6b596553c73f4b03a40b947108
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-06-17 23:41:31 +0200

    Created a stub of download page

---
+++ src/EducAction/AdesBundle/Controller/SummaryController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright (c) 2015 Tuan-Tu TRAN
+ * 
+ * This file is part of ADES.
+ * 
+ * ADES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ADES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ADES.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace EducAction\AdesBundle\Controller;
+
+use EducAction\AdesBundle\Config;
+
+class SummaryController extends Controller
+{
+    public function showAction()
+    {
+        $src=Config::LocalFile("synthese.html");
+        if(file_exists($src)){
+        } else {
+            throw $this->createNotFoundException("cette page n'existe pas");
+        }
+    }
+}
