8d0c7ea7d4119aa77a599f923380201d58b48704
Tuan-Tu Tran <tuantu.t@gmail.com>
2020-10-26 20:18:05 +0100

    fix build script + add dist project name + adapt dist compose file

--- docker/dist/build.sh
+++ docker/dist/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -t ades-base .. -f runtime-base.dockerfile
-docker build -t ades:latest -f Dockerfile ../../
+set -e #stop the script if any command fails
+
+docker build -t ades-base .. -f ../runtime-base.dockerfile
+docker build -t tuantu/ades:latest -f Dockerfile ../../
